8813caed7b4741b84207fe45aa01b2e2eaf6acae
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2021-12-23 14:50:38 -0300

    challenge sum odd number to N

---
+++ src/io/github/daylanbueno/Challenge17SumUpOddToN.java
@@ -0,0 +1,32 @@
+package io.github.daylanbueno;
+
+import java.util.Scanner;
+
+/*
+    Title: Sum of all odd numbers from 1 up to n
+    >> Write down a program that will take an integer n as input and will print
+    >> the addition of all odd numbers up to n
+    ## Test ##
+    input: 5  output: 9      tips(1+3+5=9)
+    input: 10 output: 25     tips(1+3+5+7+9=25)
+*/
+public class Challenge17SumUpOddToN {
+
+     public static void main(String [] args) {
+         Scanner in = new Scanner(System.in);
+         System.out.println("Enter an integer");
+         Integer number = in.nextInt();
+         System.out.println("result "+sumOddNumberUpToN(number));
+     }
+
+    private static Integer sumOddNumberUpToN(Integer number) {
+         int sum = 0;
+         for (int i = 1; i<= number; i ++) {
+             if (i%2!=0) {
+                 sum = sum + i;
+             }
+         }
+         return sum;
+    }
+
+}
