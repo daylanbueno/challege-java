5eeb89a66ca06a0be914178412a8e6c4790d8155
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2021-12-27 17:22:26 -0300

    converter decimal to binary

---
+++ src/io/github/daylanbueno/Challenge26DecimalToBinary.java
@@ -0,0 +1,21 @@
+package io.github.daylanbueno;
+
+import java.util.Scanner;
+
+/*
+    Title: Decimal to Binary
+    >> Write down a program that will take a decimal number as input and will print the binary equivalent
+    ## Test ##
+    input: 3 output: 11
+    input: 5 output: 101
+*/
+
+public class Challenge26DecimalToBinary {
+
+     public static void main(String [] args) {
+         Scanner in = new Scanner(System.in);
+         System.out.println("Enter an integer");
+         Integer number = in.nextInt();
+         System.out.println(Integer.toBinaryString(number));
+     }
+}
