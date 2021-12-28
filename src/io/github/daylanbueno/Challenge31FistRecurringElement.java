671df6802b56ef1a7ec80029b63ea707b191f23e
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2021-12-28 12:05:58 -0300

    result challenge fist recurring element

---
+++ src/io/github/daylanbueno/Challenge31FistRecurringElement.java
@@ -0,0 +1,28 @@
+package io.github.daylanbueno;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/*
+    Title: First recurring element in an array
+    >> Write down a program that will find first recurring element in an array
+    ## Test ##
+    input: {1, 2, 3, 4, 3}        output: 3
+    input: {1, 2, 3, 4, 3, 2, 3}  output: 2
+*/
+public class Challenge31FistRecurringElement {
+     public static void main(String [] args) {
+         Integer numbs[] = {1, 2, 3, 4, 3, 2, 3};
+         for (Integer i=1;  i < numbs.length; i++){
+             Integer value = numbs[i];
+             List<Integer> values = Arrays.stream(numbs).filter(it -> it == value).collect(Collectors.toList());
+             if (values.size() > 1) {
+                 System.out.println(value);
+                 break;
+             }
+         }
+     }
+}
